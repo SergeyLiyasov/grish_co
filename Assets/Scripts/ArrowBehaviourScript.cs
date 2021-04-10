03e06ddec1126b9bf7091c965842595084190b5f
SergeyLiyasov <liseand2002@gmail.com>
2021-04-10 16:28:23 +0500

    Создал проект
    - добавил нажимающиеся кнопки

---
+++ Assets/Scripts/ArrowBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehaviourScript : MonoBehaviour
+{
+    private SpriteRenderer Renderer;
+    public Sprite Sprite;
+    public Sprite PressedSprite; 
+    public KeyCode Key;
+
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+            Renderer.sprite = PressedSprite;
+        else if (Input.GetKeyUp(Key))
+            Renderer.sprite = Sprite;
+    }
+}
